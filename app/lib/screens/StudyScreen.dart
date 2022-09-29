507bcb6f558b369752fc0a7e850ac7be7038f578
alexeastlake <alex.eastlake@gmail.com>
2022-09-30 12:45:06 +1300

    Added working timer to study screen

--- app/lib/screens/StudyScreen.dart
+++ app/lib/screens/StudyScreen.dart
@@ -1,8 +1,52 @@
 // ignore_for_file: file_names
+import 'dart:async';
+
 import "package:flutter/material.dart";
 
-class StudyScreen extends StatelessWidget {
+class StudyScreen extends StatefulWidget {
   const StudyScreen({super.key});
+
+  @override
+  State<StudyScreen> createState() => _StudyScreenState();
+}
+
+class _StudyScreenState extends State<StudyScreen> {
+  // !!Change later, only short time for development!!
+  int studyMins = 1;
+  int breakMins = 1;
+  int time = 0;
+  Timer? _timer;
+  Duration duration = const Duration(seconds: 1);
+
+  void startStudyTimer() {
+    time = 45 * 60;
+    _timer = Timer.periodic(duration, (timer) {
+      if (time == 0) {
+        stopTimer();
+      } else {
+        setState(() {
+          time--;
+        });
+      }
+    });
+  }
+
+  void startBreakTimer() {
+    time = 15 * 60;
+    _timer = Timer.periodic(duration, (timer) {
+      if (time == 0) {
+        stopTimer();
+      } else {
+        setState(() {
+          time--;
+        });
+      }
+    });
+  }
+
+  void stopTimer() {
+    _timer?.cancel();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,12 +62,12 @@ class StudyScreen extends StatelessWidget {
             const Text(
               "Time Remaining:"
             ),
-            const Text(
-              "00:00"
+            Text(
+              "${Duration(seconds: time).inMinutes}:${time % 60}"
             ),
             TextButton(
               onPressed: (() {
-                print("Start Studying");
+                startStudyTimer();
               }),
               child: const Text(
                 "Start Studying"
@@ -31,7 +75,7 @@ class StudyScreen extends StatelessWidget {
             ),
             TextButton(
               onPressed: (() {
-                print("Start Break");
+                startBreakTimer();
               }),
               child: const Text(
                 "Start Break"
